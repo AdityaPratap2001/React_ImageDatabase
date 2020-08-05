2a6574c15b0265f7582559072b124828d2e0b00c
Aditya <pratapsinghaditya2001@gmail.com>
2020-08-05 22:42:33 +0530

    Updates

---
+++ precache-manifest.9dcf4072e270b6eb38dce2d7ae9768e1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "829a2906d956b414893264c74dff79c1",
+    "url": "/React_ImageDatabase/index.html"
+  },
+  {
+    "revision": "295130a241d7378c6dfb",
+    "url": "/React_ImageDatabase/static/css/main.b3f102e4.chunk.css"
+  },
+  {
+    "revision": "5f730ffc87af23be460f",
+    "url": "/React_ImageDatabase/static/js/2.f87f4627.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React_ImageDatabase/static/js/2.f87f4627.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "295130a241d7378c6dfb",
+    "url": "/React_ImageDatabase/static/js/main.6083264a.chunk.js"
+  },
+  {
+    "revision": "3a306453bc9fe865ce73",
+    "url": "/React_ImageDatabase/static/js/runtime-main.ad74a88c.js"
+  }
+]);
